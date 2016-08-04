4483bcc430c88f6d14377eb5c84d4602aba18a7d
luliqzx <luliqzx@gmail.com>
2016-08-04 07:22:37 +0700

    initialize

---
+++ app/Http/routes.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+	return view('welcome');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
+
+Route::get('/about', 'HomeController@showAbout');
+
+
+Route::get('/testmodel', function() {
+$post = new App\Post;
+$post->title = "7 Amalan Pembuka Jodoh";
+ $post->content = "shalat malam, sedekah, puasa sunah, silaturahmi, senyum, doa, tobat";
+ $post->save();
+ return $post;
+ // check record baru di database
+});
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
